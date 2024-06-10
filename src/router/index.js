dc144acfea91ee2b5034c2103f030406cb4ed662
Peng.Xu13 <Peng.Xu13@zeekrlife.com>
2024-06-10 16:31:43 +0800

    feat: init

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,18 @@
 import VueRouter from "vue-router";
-
+import Test from "../views/Test.vue";
+import Test1 from "../views/Test1.vue";
 const router = new VueRouter({
-  routes: [{}],
+  mode: "hash",
+  routes: [
+    {
+      path: "/",
+      component: Test,
+    },
+    {
+      path: "/test1",
+      component: Test1,
+    },
+  ],
 });
 
 export default router;
---
+++ src/views/Test1.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    This is Test Page
+    <button @click="jump">跳转</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    jump() {
+      this.$router.push("/test1");
+    },
+  },
+};
+</script>
